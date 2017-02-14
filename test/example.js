d02c73d4d0d00f29b0b21f7ef55f5f01f407b5bf
Micah Zoltu <micah@zoltu.net>
2017-02-14 09:38:20 -0800

    Adds a terrible workaround to the problem.

--- test/example.js
+++ test/example.js
@@ -1,6 +1,10 @@
 const assert = require("chai").assert;
+const originalUncaughtExceptionListeners = process.listeners("uncaughtException")
 // comment out the following line to get the test runner to do the right thing
 const solc = require("solc");
+// uncomment the following lines to "fix" the problem caused by the line above
+//process.removeAllListeners("uncaughtException");
+//originalUncaughtExceptionListeners.forEach((listener) => process.addListener("uncaughtException", listener));
 
 describe("example", () => {
   it("first", (done) => setTimeout(() => assert.fail("first failure")));
