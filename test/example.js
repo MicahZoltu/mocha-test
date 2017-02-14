cab36d215222c6bf755f952c4555004dcf8ca7b9
Micah Zoltu <micah@zoltu.net>
2017-02-13 22:41:38 -0800

    Makes tests fail.

--- test/example.js
+++ test/example.js
@@ -1,4 +1,5 @@
-var assert = require("chai").assert;
+const assert = require("chai").assert;
+const TestRPC = require("ethereumjs-testrpc");
 
 describe("example", () => {
   it("first", (done) => setTimeout(() => assert.fail("first failure")));
